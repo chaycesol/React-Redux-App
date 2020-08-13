25d6edf19d5906c7b98f70e73cbc202d2aa4e7ea
chaycesol <chayce.solchaga@gmail.com>
2020-08-12 23:19:28 -0700

    Everything up and running just need to style

--- app/src/components/Characters.js
+++ app/src/components/Characters.js
@@ -4,20 +4,34 @@ import { fetchCharacters } from '../store';
 
 const Characters = (props) => {
     useEffect(() => {
-        fetchCharacters()
+        props.fetchCharacters();
     }, [])
+    
     return(
         <section>
-            <h3>Rick and Morty Present</h3>
-            <h1>The Galactic Federation Most Wanted</h1>
-            {props.isLoading ? <h4>Loading missions now... </h4> : null}
+            <img src="https://www.seekpng.com/png/full/112-1129023_no-account-no-worries-rick-and-morty-logo.png" />
+            <h3>Present</h3>
+            <h1>The Galactic Federation's Most Wanted</h1>
+            {props.isLoading ? <h4>Retrieving List of Criminals now... </h4> : null}
       {props.error ? (
-        <p style={{ color: "red" }}> My bad. Launch cancelled! {props.error}</p>
+        <p style={{ color: "red" }}> Error: Connection Failed - BE ON THE LOOKOUT FOR A RICK! {props.error}</p>
       ) : null}
       {props.characters.length > 0 ? (
         <div>
+            <h2>ARREST ON SIGHT: </h2>
           {props.characters.map((character) => (
-            <h2 key={character.id}>{character.name}</h2>
+            <span key={character.id}>
+                <h3>FUGITIVE: {character.name}</h3>
+                <img src={character.image}/>
+                    <p>Species: {character.species}</p>
+                    <p>Origin: {character.origin.name}</p>
+                    <p>Last Known Status: {character.status}</p>
+                    <p>Recent Sightings</p>
+                    {character.episode.map((sighting, index) =>(
+                        <li><a href={sighting} key={index}>Sighting Data</a></li>
+                    )
+                    )}
+            </span>
           ))}
           </div>
       ) : null}
@@ -33,4 +47,4 @@ const mapStateToProps = (state) => {
     };
 };
 
-export default connect(mapStateToProps, {})(Characters);
+export default connect(mapStateToProps, {fetchCharacters})(Characters);
--- app/src/store/actions/rickActions.js
+++ app/src/store/actions/rickActions.js
@@ -3,28 +3,21 @@ import axios from 'axios'
 export const FETCHING_CHARACTERS_START = "FETCHING_CHARACTERS_START";
 export const FETCH_CHARACTERS_SUCCESS = "FETCH_CHARACTERS_SUCCESS";
 
-export const fetchCharacters = () => {
-
-    return (dispatch) => {
+export const fetchCharacters = () => (dispatch) => {
         dispatch({ type: FETCHING_CHARACTERS_START });
         axios
-        .get("https://rickandmortyapi.com/api/character")
-        .then((res) => {
-            console.log(res)
-            dispatch({ type: FETCH_CHARACTERS_SUCCESS, payload: res.data.data})
-        }) 
-        .catch((err) => 
-            console.log(err)
-        );
-
-    }
-}
+            .get("https://rickandmortyapi.com/api/character")
+            .then((res) => {
+                dispatch({ type: FETCH_CHARACTERS_SUCCESS, payload: res.data.results})
+            }) 
+            .catch((err) => console.log(err));
+    };
 
 //redux-thunk
-const thunk = (store) => (next)=> (action) => {
-    if (typeof action === 'object') {
-        next(action);
+const thunk = (store) => (next) => (action) => {
+    if (typeof action === "object") {
+      next(action);
     } else if (typeof action === "function") {
-        action(store.dispatch);
+      action(store.dispatch);
     }
-}
+  };
